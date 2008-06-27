8a62fce179bb0be21c99d09160bbf677a30b4ae4
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-06-27 21:24:57 +0000

    new real hexagons and icon parts rc1=release candidate 1

--- alleg/octagontest/testoct.pl
+++ alleg/octagontest/testoct.pl
@@ -9,12 +9,20 @@ open(OUTPUTPNG,">constellationoctagon.png");
 $img = GD::Simple->new(1000,1300);
 
 $img1 = newFromGif GD::Image('constellations.gif');
-$img2 = newFromGif GD::Image('octagontestcolordh0.gif');
-$img3 = newFromGif GD::Image('OctetPartBlue.gif');
+$img2 = newFromGif GD::Image('octagon_empty_sample.gif');
+$img3 = newFromGif GD::Image('octagon_filled_sector_1.gif');
+$img4 = newFromGif GD::Image('octagon_filled_sector_6.gif');
 
 $img->copy($img1,0,0,0,0,1000,1300);
 $img->copy($img2,0,0,0,0,300,300); 
-$img->copy($img3,0,0,0,0,99,139); 
+$img->copy($img3,0,0,0,0,300,300); 
+$img->copy($img4,0,0,0,0,300,300); 
+
+
+
+$img->copy($img2,500,500,0,0,300,300); 
+$img->copy($img3,500,500,0,0,300,300); 
+$img->copy($img4,500,500,0,0,300,300); 
 
 
 print OUTPUTPNG $img->png;
