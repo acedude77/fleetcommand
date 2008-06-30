b180c72503432d0a4dd8653d788d6bd5bfa7f111
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-06-30 09:43:36 +0000

    coloring by manual pix selection

--- alleg/rc1/sector3/colorsector3.pl
+++ alleg/rc1/sector3/colorsector3.pl
@@ -8,19 +8,47 @@ open(OUTPUTPNG,">output.png");
 # create a new image
 $img = GD::Simple->new(300,300);
 
-$img1 = newFromGif GD::Image('examplemoneysettingsgb3.gif');
+$img1 = newFromGif GD::Image('example1ed7.gif');
 
 $img->copy($img1,0,0,0,0,300,300);
 
 # allocate some colors
 #$white = $img->colorAllocate(255,255,255);
-#$black = $img->colorAllocate(0,0,0);       
-#$red = $img->colorAllocate(255,0,0);      
+$black = $img->colorAllocate(0,0,0);       
+$red = $img->colorAllocate(255,0,0);      
 $blue = $img->colorAllocate(0,0,255);
 
-$img->fill(218,157,$blue);
+#tac
+$img->fill(196,157,$blue);
+#exp
+$img->fill(196,163,$black);
+#sup
+$img->fill(196,169,$red);
 
+#tr
+$img->fill(223,157,$red);
+$img->fill(223,163,$red);
+$img->fill(223,169,$red);
 
+#t
+$j=0;
+while($j<8){
+	$img->fill(235,157+($j*6),$red);
+	$j++;
+}
 
+#r
+$j=0;
+while($j<8){
+	$img->fill(247,157+($j*6),$blue);
+	$j++;
+}
+
+#s
+$j=0;
+while($j<7){
+	$img->fill(260,157+($j*6),$blue);
+	$j++;
+}
 
 print OUTPUTPNG $img->png;
