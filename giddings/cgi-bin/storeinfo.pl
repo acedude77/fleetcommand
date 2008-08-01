3f167aa7c1eda69d332059dd7f301794ed52bf2a
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-08-01 07:37:31 +0000

    link to output.txt and some minor edits

--- giddings/cgi-bin/storeinfo.pl
+++ giddings/cgi-bin/storeinfo.pl
@@ -14,7 +14,7 @@ $y1=$y-15;
 $x2=$x+16;
 $y2=$y+14;
 
-open(OUTPUT,">>output.txt");
+open(OUTPUT,">>./output.txt");
 
 print OUTPUT qq^<area shape=rect coords=\"$x1,$y1,$x2,$y2\" href="javascript:alert('$name')">\n^;
 print qq^<meta http-equiv="REFRESH" content="0;url=../index.html">^; 
