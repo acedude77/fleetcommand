932ecd4962edfdc1e4a616fc03b279b7de9b1ca2
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-08-09 13:01:22 +0000

    generate_octagons.pl works probably need to explore how to label stuff later
    javascript for mouseover also done
    
    sql.dump is the database export

--- alleg/ui/octagon_configurator/generate_octagons.pl
+++ alleg/ui/octagon_configurator/generate_octagons.pl
@@ -3,6 +3,7 @@
 use DBI;
 use GD;
 use GD::Simple;
+require "/home/jctong/workarea/afcmg/testoct.pm";
 
 my $user='root';
 my $pass='';
@@ -10,13 +11,17 @@ my $dsn='dbi:mysql:alleg:localhost:3306';
 my $dbh=DBI->connect($dsn,$user,$pass);
 
 
-my $sth=$dbh->prepare('select sector_name,available_techs,treasures,total_cash,resources,starting_cash from afcoc where id=1');
+my $sth=$dbh->prepare('select sector_name,available_techs,treasures,total_cash,resources,starting_cash from afcoc');
 
 $sth->execute();
 
 while(($sector_name,$available_techs,$treasures,$total_cash,$resources,$starting_cash)=$sth->fetchrow_array()){
 	$filename=$sector_name.".png";
 	open(OUTPUT,">$filename");
-	my $img = newFromGif GD::Image('/home/jctong/workarea/afcmg/blancOctagon.gif');
+	$img = newFromGif GD::Image('/home/jctong/workarea/afcmg/sector3Grey.gif');
+	$img->copy(newFromGif GD::Image('/home/jctong/workarea/afcmg/sector3EmptyMoneyTechIcons.gif'),0,0,0,0,300,300);
+
+	&moneysetting2(0,0,$available_techs,$treasures,$total_cash,$resources,$starting_cash);
+
 	print OUTPUT $img->png;
 }
