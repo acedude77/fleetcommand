b0f578a15f6a1af63399923c249e157e547433cd
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-08-25 07:30:07 +0000

    added visibility column for showdb and included ability to delete visibility data

--- alleg/ui/octagon_configurator/bin/showdb.pl
+++ alleg/ui/octagon_configurator/bin/showdb.pl
@@ -7,7 +7,7 @@ my $pass='pZbDdzuW.tmXMvqX';
 my $dsn='dbi:mysql:alleg:localhost:3306';
 my $dbh=DBI->connect($dsn,$user,$pass);
 
-my $sth=$dbh->prepare('select id as sectorid,sector_name,backgrounds,icons from afcoc order by timestamp desc');
+my $sth=$dbh->prepare('select id as sectorid,sector_name,visibility,backgrounds,icons from afcoc order by timestamp desc');
 $sth->execute();
 
 print header,start_html;
@@ -16,10 +16,10 @@ print "<a href='../generate_octagons.pl'>generate octagons</a><br>";
 print "<a href='display.pl'>back to global map</a>";
 
 print "<table border='1'>\n";
-print "<tr><th>sector id</th><th>sector name</th><th>backgrounds</th><th>icons</th></tr>\n";
+print "<tr><th>sector id</th><th>sector name</th><th>visibility</th><th>backgrounds</th><th>icons</th></tr>\n";
 
-while(($sectorid,$sector_name,$backgrounds,$icons)=$sth->fetchrow_array()){
-	print "<tr><td><a href='cleardb.pl?$sectorid'>$sectorid</a></td><td>$sector_name</td><td>$backgrounds</td><td>$icons</td></tr>\n";
+while(($sectorid,$sector_name,$visibility,$backgrounds,$icons)=$sth->fetchrow_array()){
+	print "<tr><td><a href='cleardb.pl?$sectorid'>$sectorid</a></td><td>$sector_name</td><td>$visibility</td><td>$backgrounds</td><td>$icons</td></tr>\n";
 }
 
 print "</table>\n";
