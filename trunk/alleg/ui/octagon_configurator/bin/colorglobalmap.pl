3267a7dbd7c82bacf1347c8efb861d6b64101db4
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-08-26 10:48:10 +0000

    succesfully colored a slice in global map

--- trunk/alleg/ui/octagon_configurator/bin/colorglobalmap.pl
+++ trunk/alleg/ui/octagon_configurator/bin/colorglobalmap.pl
@@ -16,29 +16,38 @@ my $sth=$dbh->prepare('select sector_name,backgrounds,visibility,location from a
 
 $sth->execute();
 
-while(($sector_name,$backgrounds,$visibility,$location)=$sth->fetchrow_array()){
 
+$img = newFromGif GD::Image('../map1024tbak2.gif');
+open(OUTPUT,">/tmp/a.png");
 
-	$img = newFromGif GD::Image('/home/jctong/workarea/afcmg/sector3Grey.gif');
-	$img->copy(newFromGif GD::Image('/home/jctong/workarea/afcmg/sector3EmptyMoneyTechIcons.gif'),0,0,0,0,300,300);
+#$img->copy(newFromGif GD::Image('/home/jctong/workarea/afcmg/sector3EmptyMoneyTechIcons.gif'),0,0,0,0,300,300);
 
+my $yellow = $img->colorAllocate(255,250,205);
+my $blue = $img->colorAllocate(0,0,255);
+
+while(my($sector_name,$backgrounds,$visibility,$location)=$sth->fetchrow_array()){
 
 	@backgrounds=split(/ /,$backgrounds);
 	foreach my $background (@backgrounds){
 		$background =~s/^\s//;
 		$background =~s/\s$//;
 		if($background ne ""){
-			
-			$img->copy(newFromGif GD::Image($file),0,0,0,0,300,300);
+			$background=~/sector(\d)Background(\w+)\.gif/;
+			my($x,$y)=split(/,/,$location);
+			$fillx=$x-10;
+			$filly=$y-5;
+			$img->fill($fillx,$filly,$blue);
+			print "$sector_name $x $y $fillx $filly done\n";
 		}
 	}
 
 
-	print OUTPUT $img->png;
 }
 
-print header,start_html;
-print "octagons generated<br>";
-print "<a href='bin/display.pl'>back to global map</a><br>";
-print "<a href='bin/showdb.pl'>back to show db</a><br>";
-print end_html;
+print OUTPUT $img->png;
+
+#print header,start_html;
+#print "octagons generated<br>";
+#print "<a href='bin/display.pl'>back to global map</a><br>";
+#print "<a href='bin/showdb.pl'>back to show db</a><br>";
+#print end_html;
