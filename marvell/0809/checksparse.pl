0107a7503cc921a8db0b6d5eb91b322a6dd04fff
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-09-11 22:21:01 +0000

    script to identify sparse (holey) files

---
+++ marvell/0809/checksparse.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use File::Find;
+sub process_file {
+         my $f=$File::Find::name;
+         (my $dev,my $ino,my $mode,my $nlink,my $uid,my $gid,my $rdev,my $size,my $atime,my $mtime,my $ctime,my $blksize,my $blocks) =stat($f);
+         if ($blocks * 512 < $size) {
+                 print "$f => SZ: $size BLSZ: $blksize BLKS: $blocks\n";
+                 #print "\t" . -s $f;
+         }
+}
+find(\&process_file,@ARGV);
