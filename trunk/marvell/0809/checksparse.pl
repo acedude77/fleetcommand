5693aba991acd13a0c39722143b25ed8dc1d0366
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-09-11 22:21:06 +0000

    added help output

--- trunk/marvell/0809/checksparse.pl
+++ trunk/marvell/0809/checksparse.pl
@@ -1,6 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
 use warnings;
+
+if(!defined(@ARGV)){
+	print "$0 <full path>\n";
+	exit;
+}
+
 use File::Find;
 sub process_file {
          my $f=$File::Find::name;
