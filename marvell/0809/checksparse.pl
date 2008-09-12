729d7ebf38457c039aade0525f49538fc1c338ec
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-09-12 00:42:57 +0000

    fixed checksparse.pl to only process files (skips directories and symlinks) results : results from test run

--- marvell/0809/checksparse.pl
+++ marvell/0809/checksparse.pl
@@ -10,10 +10,12 @@ if(!defined(@ARGV)){
 use File::Find;
 sub process_file {
          my $f=$File::Find::name;
-         (my $dev,my $ino,my $mode,my $nlink,my $uid,my $gid,my $rdev,my $size,my $atime,my $mtime,my $ctime,my $blksize,my $blocks) =stat($f);
-         if ($blocks * 512 < $size) {
-                 print "$f => SZ: $size BLSZ: $blksize BLKS: $blocks\n";
-                 #print "\t" . -s $f;
-         }
+	if(-f){
+		(my $dev,my $ino,my $mode,my $nlink,my $uid,my $gid,my $rdev,my $size,my $atime,my $mtime,my $ctime,my $blksize,my $blocks) =stat($f);
+		my $space_used=$blocks*512;
+         	if ($space_used < $size) {
+                 	print "$f => size: $size   actual space used: $space_used\n";
+         	}
+	}
 }
 find(\&process_file,@ARGV);
