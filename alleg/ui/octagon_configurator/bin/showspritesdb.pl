4c6608351e7112be91bfb7f0d374840eb8c5e0e9
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-09-18 11:45:43 +0000

    more mines/sprite implementation

---
+++ alleg/ui/octagon_configurator/bin/showspritesdb.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl -w
+use CGI qw(:standard);
+use DBI;
+
+my $user='alleguser';
+my $pass='pZbDdzuW.tmXMvqX';
+my $dsn='dbi:mysql:alleg:localhost:3306';
+my $dbh=DBI->connect($dsn,$user,$pass);
+
+my $sth=$dbh->prepare('select id,location,sprite_type,team from sprites order by timestamp desc');
+$sth->execute();
+
+print header,start_html;
+
+print "<a href='../generate_octagons.pl'>generate octagons</a><br>";
+print "<a href='display.pl'>back to global map</a><br>";
+print "<a href='colorglobalmap.pl'>update global overlay</a><br>";
+
+print "<table border='1'>\n";
+print "<tr><th>id</th><th>location</th><th>sprite_type</th><th>team</th></tr>\n";
+
+while(($id,$location,$sprite_type,$team)=$sth->fetchrow_array()){
+	print "<tr><td><a href='clearspritesdb.pl?$id'>$id</a></td><td>$location</td><td>$sprite_type</td><td>$team</td></tr>\n";
+}
+
+print "</table>\n";
+
+print end_html;
