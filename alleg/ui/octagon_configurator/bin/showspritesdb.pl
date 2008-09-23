50f9f57d24277e34aaf44c1f71c0edff5da75052
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2008-09-23 10:28:23 +0000

    more progress...

--- alleg/ui/octagon_configurator/bin/showspritesdb.pl
+++ alleg/ui/octagon_configurator/bin/showspritesdb.pl
@@ -7,7 +7,7 @@ my $pass='pZbDdzuW.tmXMvqX';
 my $dsn='dbi:mysql:alleg:localhost:3306';
 my $dbh=DBI->connect($dsn,$user,$pass);
 
-my $sth=$dbh->prepare('select id,location,sprite_type,team from sprites order by timestamp desc');
+my $sth=$dbh->prepare('select id,location,sprite_type,team,visibility from sprites order by timestamp desc');
 $sth->execute();
 
 print header,start_html;
@@ -17,10 +17,10 @@ print "<a href='display.pl'>back to global map</a><br>";
 print "<a href='colorglobalmap.pl'>update global overlay</a><br>";
 
 print "<table border='1'>\n";
-print "<tr><th>id</th><th>location</th><th>sprite_type</th><th>team</th></tr>\n";
+print "<tr><th>id</th><th>location</th><th>sprite_type</th><th>team</th><th>visibility</th></tr>\n";
 
-while(($id,$location,$sprite_type,$team)=$sth->fetchrow_array()){
-	print "<tr><td><a href='clearspritesdb.pl?$id'>$id</a></td><td>$location</td><td>$sprite_type</td><td>$team</td></tr>\n";
+while(($id,$location,$sprite_type,$team,$visibility)=$sth->fetchrow_array()){
+	print "<tr><td><a href='clearspritesdb.pl?$id'>$id</a></td><td>$location</td><td>$sprite_type</td><td>$team</td><td>$visibility</td></tr>\n";
 }
 
 print "</table>\n";
