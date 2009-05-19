238abb966600692450bed225c65e9766ccbc911f
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2009-05-19 06:40:32 +0000

    checkpoint

--- scripts/asparser/parse.pl
+++ scripts/asparser/parse.pl
@@ -11,7 +11,13 @@ my $dbh=DBI->connect('DBI:mysql:allegskill','root','asdfjk');
 
 $dbh->do("update stats set status=0");
 
+my $lgp=0;
+
 while(my $line=<INPUT>){
+	if($line=~/LastGameProcessedLabel">(\d+)</){
+		$lgp=$1;
+	}
+
 	if($line=~/<\/td><td align="right">/){
 		$line=~s/<\/td>/ /g;
 		$line=~s/<td>/ /g;
@@ -28,15 +34,37 @@ while(my $line=<INPUT>){
 
 system("mv /tmp/index.html /tmp/index.html.old");
 
-my $columns="callsign,drn_kills";
+my $columns="callsign,rank,sigma,drn_kills,stn_caps";
+#my $callsigns="fwiffo,merc";
+my $callsigns="fwiffo,weylin,merc,cashto,podkiller,kltplzyxm,chiveicrook,ryjamsan";
+
+my @callsign_array=split(/,/,$callsigns);
+my $callsigns_str="";
+foreach my $callsign (@callsign_array){
+	$callsigns_str=$callsigns_str."callsign=\"$callsign\" or ";
+}
+$callsigns_str=~s/ or $//;
+
 #my $sth=$dbh->prepare("select $columns from stats where status=1 order by drn_kills desc limit 10");
-my $sth=$dbh->prepare("select $columns from stats where status=1 order by drn_kills desc limit 10");
+my $sth=$dbh->prepare("select $columns from stats where status=1 AND ($callsigns_str) order by drn_kills desc");
 $sth->execute();
 
 my @cols=split(/,/,$columns);
 
 print header;
 print start_html;
+
+print <<END;
+<form action="parse.pl" method="post">
+<p>columns <input type="text" name="icolumns" value="$columns" size="80"></p><br>
+<p>callsigns <input type="text" name="icallsigns" value="$callsigns" size="80"></p><br>
+<input type="submit" name="submit" value="Submit">
+</form>
+END
+
+
+print "<hr>";
+print "Last Game Processed: $lgp<br>";
 print "<table border=1>";
 
 print "<tr>";
