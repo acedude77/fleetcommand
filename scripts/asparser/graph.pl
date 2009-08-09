d250641bfda3b26e136d9d2c490130dd9e65e0d8
borgified <borgified@f80588c6-1150-0410-9f68-517f3a15f869>
2009-08-09 06:22:05 +0000

    v1.0 finished

--- scripts/asparser/graph.pl
+++ scripts/asparser/graph.pl
@@ -1,7 +1,17 @@
+
+
 #!/usr/bin/perl -w
 use Chart::Strip;
 use DBI;
 use Date::Manip;
+use strict;
+
+sub graph{
+my($stat,$duration,@callsigns)=@_;
+
+#$stat="drn_kills";
+#$duration="past_week";
+#@callsigns=("pkunk","champyx","cashto");
 
 my $db="allegskill";
 my $user="root";
@@ -11,53 +21,73 @@ my $dbh  = DBI->connect ("DBI:mysql:database=$db:host=localhost",
         $user,
         $password)
         or die "Can't connect to database: $DBI::errstr\n";
-my $sth;
 
-$sth=$dbh->prepare('select timestamp,kills_per_ejects,kills_per_hr,rank,wins,losses,drn_kills,hrs_played from stats where callsign = \'champyx\' AND timestamp > (now() - interval 1 week) order by timestamp');
-$sth->execute();
-
-my($data,$data1,$data2,$data3,$data4);
-
-while(my($timestamp,$kills_per_ejects,$kills_per_hr,$rank,$wins,$losses,$drn_kills,$hrs_played)=$sth->fetchrow_array()){
-	$unixdate=UnixDate(ParseDate($timestamp),"%s");
-	#print "$unixdate $rank\n";
-	push @$data, {time => $unixdate, value => $kills_per_ejects};
-	push @$data1, {time => $unixdate, value => $kills_per_hr};
-	push @$data2, {time => $unixdate, value => $rank};
-	#push @$data3, {time => $unixdate, value => $drn_kills/($wins+$losses)};
-	push @$data3, {time => $unixdate, value => $drn_kills/$hrs_played};
-
-}
-
-
-$sth=$dbh->prepare('select timestamp,kills_per_ejects,kills_per_hr,rank,wins,losses,drn_kills,hrs_played from stats where callsign = \'champyx\' AND timestamp > (now() - interval 1 week) order by timestamp');
-$sth->execute();
-
-my $once=1;
-my $drn_kills_sow=0;
-while(my($timestamp,$kills_per_ejects,$kills_per_hr,$rank,$wins,$losses,$drn_kills,$hrs_played)=$sth->fetchrow_array()){
-	if($once==1){
-		$drn_kills_sow=$drn_kills;
-		$once=0;
-	}
-	$drn_kills=$drn_kills-$drn_kills_sow;
-	$unixdate=UnixDate(ParseDate($timestamp),"%s");
-	push @$data4, {time => $unixdate, value => $drn_kills};
-}
-
-
-my $img = Chart::Strip->new(title   => 'drone_kills',
+my $img = Chart::Strip->new(title   => $stat,
 			    x_label => '',
 			    y_label => '',
 			    transparent => 0,
 			    );
 
-#$img->add_data( $data, { label => 'kills_per_ejects', style => 'line',   color => '802000' } );
-#$img->add_data( $data1, { label => 'kills_per_hr', style => 'line',   color => 'FF0000' } );
-#$img->add_data( $data2, { label => 'rank', style => 'line',   color => '00FF00' } );
-#$img->add_data( $data3, { label => 'champyx', style => 'line',   color => '0000FF' } );
-$img->add_data( $data4, { label => 'champyx', style => 'points',   color => 'FF0000' } );
+my $color;
+my @colors=("000000","0000FF","00FF00","00FFFF","FF0000","FF6600","FF00FF");
+my $num_colors=@colors;
 
-print $img->png();
+    sub fisher_yates_shuffle {
+        my $deck = shift;  # $deck is a reference to an array
+        my $i = @$deck;
+        while ($i--) {
+            my $j = int rand ($i+1);
+            @$deck[$i,$j] = @$deck[$j,$i];
+        }
+    }
 
+&fisher_yates_shuffle(\@colors);
 
+foreach my $callsign (@callsigns) {
+
+my $sth;
+my $duration_str;
+
+if($duration eq 'past_week'){
+	$duration_str='AND timestamp > (now() - interval 1 week)';
+}elsif($duration eq 'past_month'){
+	$duration_str='AND timestamp > (now() - interval 1 month)';
+}else{
+	$duration_str=' ';
+}
+
+my $query="select timestamp,".$stat." from stats where callsign=\'".$callsign."\' ".$duration_str." order by timestamp";
+
+$sth=$dbh->prepare($query);
+
+$sth->execute();
+
+my($data);
+my $once=1;
+my($zeroed_stat)=0;
+
+while(my($timestamp,$chosen_stat)=$sth->fetchrow_array()){
+	if($once==1 && $duration =~ /past_/ ){
+		$zeroed_stat=$chosen_stat;
+		$once=0;
+	}
+	$chosen_stat=$chosen_stat-$zeroed_stat;
+ 
+	my $unixdate=UnixDate(ParseDate($timestamp),"%s");
+	push @$data, {time => $unixdate, value => $chosen_stat};
+}
+
+$color=pop(@colors);
+if(!defined($color)){
+	print "too many callsigns (>$num_colors), ran out of colors!\n";
+	exit;
+}
+$img->add_data( $data, { label => $callsign, style => 'line',   color => $color } );
+}
+
+open(OUTPUT,">/tmp/a.png");
+
+print OUTPUT $img->png();
+
+}
+1;
--- scripts/asparser/stats.pl
+++ scripts/asparser/stats.pl
@@ -1,17 +1,69 @@
 #!/usr/bin/perl -w
 
 use CGI qw(:standard);
+require "/home/jctong/svn/scripts/asparser/graph.pl";
+
+$query=new CGI;
+unless($action=$query->param('submit')){
+	$action='none';
+}
+
 
 print header;
 print start_html;
 
+$callsigns=$query->param('callsigns');
+$stat=$query->param('stat');
+
 print <<END;
 <form action="stats.pl" method="post">
-<p>columns <input type="text" name="icolumns" value="$columns" size="80"></p><br>
-<p>callsigns <input type="text" name="icallsigns" value="$callsigns" size="80"></p><br>
-<input type="submit" name="submit" value="Submit">
+<p>use comma to separate multiple callsigns
+<p>callsigns <input type="text" name="callsigns" value="$callsigns" size="80"></p><br>
+<p>pick a stat: <select name="stat">
+<option value="mu">Mu</option>
+<option value="sigma">Sigma</option>
+<option value="rank">Rank</option>
+<option value="wins">Wins</option>
+<option value="losses">Losses</option>
+<option value="draws">Draws</option>
+<option value="stack_rating">Stack Rating</option>
+<option value="cmd_mu">Cmd Mu</option>
+<option value="cmd_sigma">Cmd Sigma</option>
+<option value="cmd_rank">Cmd Rank</option>
+<option value="cmd_wins">Cmd Wins</option>
+<option value="cmd_lossses">Cmd Losses</option>
+<option value="cmd_draws">Cmd Draws</option>
+<option value="kills">Kills</option>
+<option value="ejects">Ejects</option>
+<option value="drn_kills">Drn Kills</option>
+<option value="stn_kills">Stn Kills</option>
+<option value="stn_caps">Stn Caps</option>
+<option value="kills_per_ejects">Kills per Eject</option>
+<option value="hrs_played">Hours Played</option>
+<option value="kills_per_hr">Kills per Hour</option>
+</select>
+
+
+<p>select a duration: <select name="duration">
+<option value="past_week">Past Week</option>
+<option value="past_month">Past Month</option>
+<option value="all">Show All</option>
+</select>
+<p>
+<p><input type="submit" name="submit" value="submit">
 </form>
 END
 
+if($action eq 'submit'){
+	$callsigns=$query->param('callsigns');
+	$stat=$query->param('stat');
+	$duration=$query->param('duration');
 
-print "</html>";
+	$callsigns=~s/\s+//g;
+	my @callsigns_array=split(/,/,$callsigns);
+	&graph($stat,$duration,@callsigns_array);
+	print '<hr><img src="/asparser/a.png">';
+}
+
+
+print "</body></html>";
